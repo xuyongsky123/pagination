2c0718a72ea024b400f5186239173b37391fae80
Kevin Xu (xuyong) <xuyongsky123@outlook.com>
2016-03-30 12:15:36 +0800

    repair bug Edit by xuyong - 2016/3/30

--- js/jquery.pagination.js
+++ js/jquery.pagination.js
@@ -4,6 +4,7 @@ var _flag = true; //解决scroll多次执行事件BUG，不能丢
 	//默认参数及其介绍
 	var defaults = {
 		num:5,		//一次加载个数
+		singleHeight:45, //单个循环dom高度，用于初次加载初始化（计算刚刚开始需要加载次数）
 		ajaxEvent:function(me){
 			$.noop();
 		},
@@ -11,6 +12,12 @@ var _flag = true; //解决scroll多次执行事件BUG，不能丢
 
 
 	$.fn.pagination = function(options){
+		//判断循环个体高度设置情况
+		if(!options.singleHeight){
+			alert('error ! singleHeight 为必须配置参数');
+			return;
+		}
+
 		var params = $.extend(defaults,options);
 		return new paginationInner(this,params);
 	}
@@ -29,8 +36,15 @@ var _flag = true; //解决scroll多次执行事件BUG，不能丢
 			}
 		},options);
 
+
+		//初始化加载单个循环dom高度判断
+		if(isNaN(me.opts.singleHeight)){
+			alert('error ! singleHeight 值不能为非数字');
+			return;
+		}
+
 	    //初始化加载
-	    var initTotalNum = Math.ceil(Math.ceil(($(ele).height()/45)/me.opts.num));
+	    var initTotalNum = Math.ceil(Math.ceil(($(ele).height()/me.opts.singleHeight)/me.opts.num));
 		var _inter = setInterval(function(){
 			if(initTotalNum > 0){
 				me.opts.ajaxEvent(me);
